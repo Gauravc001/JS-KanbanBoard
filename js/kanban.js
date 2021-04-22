6518e475267f5f0a586e417318b1d6bdf4ec1c26
GauravC001 <GauravKumarSahu@eurofins.com>
2021-04-22 20:19:09 +0530

    Create item functionality added; Drag and drop of item implemented;

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,56 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  const item = document.createElement('div');
+  item.classList.add('item');
+  item.setAttribute('id', 'item-' + order);
+  item.setAttribute('draggable', true);
+  item.addEventListener('dragstart', dragStart);
+  item.addEventListener('dragend', dragEnd);
+
+  const input = document.createElement('input');
+  item.appendChild(input);
+
+  const save_btn = document.createElement('button');
+  save_btn.innerHTML = "Save";
+  save_btn.addEventListener('click', onclick);
+  item.appendChild(save_btn);
+
+  return item;
 };
 
+const dragStart = (event) => {
+  event.dataTransfer.setData("Text", event.target.id);
+}
+const dragEnd = (event) => {
+  event.dataTransfer.clearData();
+}
+
+const onDrop = (event) => {
+  const id = event.dataTransfer.getData("Text");
+  const item = document.getElementById(id);
+  event.target.appendChild(item);
+  event.preventDefault();
+}
+
+const onDragover = (event) => {
+  event.preventDefault();
+}
+
+const onclick = () => {
+  error.innerHTML = "";
+  const input = document.querySelector("input");
+  if(input !== ""){
+    const item = document.querySelector("#item-" + order)
+    order += 1;
+    item.innerHTML = input.value;
+    adding = false;
+  } else {
+    error.innerHTML = message;
+  }
+}
+
 document.querySelectorAll('.drop').forEach(element => {
+  element.addEventListener('drop', onDrop);
+  element.addEventListener('dragover', onDragover)
 });
